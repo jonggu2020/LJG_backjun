e019b15607d9dfe475d339ef7ecec4b2a3d1806c
jonggu2020 <75966566+jonggu2020@users.noreply.github.com>
2024-01-10 15:51:48 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,5 +1,5 @@
 num = int(input())
 for a in range(1, num):
-    print(' ' * (num - a) + '*' *(2 * a - 1))
+    print(' ' * (num - a) + '*' *(2 * a - 1))        #공백부여하여 삼각형 만들기
 for b in range(num, 0, -1):
-    print(' ' * (num - b) + '*' *(2 * b - 1))
+    print(' ' * (num - b) + '*' *(2 * b - 1))        #역순으로 하여 역삼각형 만들기
